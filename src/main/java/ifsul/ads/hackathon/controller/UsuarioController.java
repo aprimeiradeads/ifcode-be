f4588e084ec0b561d46f0c7e26656ca9dc8e72c8
jpedro-m <joaopedromarques2323@gmail.com>
2025-09-13 11:09:22 -0300

    hot fix

--- src/main/java/ifsul/ads/hackathon/controller/UsuarioController.java
+++ src/main/java/ifsul/ads/hackathon/controller/UsuarioController.java
@@ -1,18 +1,15 @@
 package ifsul.ads.hackathon.controller;
 
-import java.util.List;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ifsul.ads.hackathon.domain.entity.Usuario;
 import ifsul.ads.hackathon.service.UsuarioService;
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -22,8 +19,9 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping("/auth")
-    public ResponseEntity<String> login(@RequestBody String idToken) {
+    public ResponseEntity<?> login(@RequestBody String idToken) {
         String jwt = usuarioService.login(idToken);
-        return ResponseEntity.ok(jwt);
+        
+        return ResponseEntity.ok(Collections.singletonMap("token", jwt));
     }
 }
